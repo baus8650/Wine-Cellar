ccebc84b38a59d3a3c76dfa489db69e8822e6cea
Tim Bausch <49855076+baus8650@users.noreply.github.com>
2022-10-22 17:49:29 -0700

    Comment builder files temporarily

--- Wine CellarTests/Builder Files/VineyardBuilder.swift
+++ Wine CellarTests/Builder Files/VineyardBuilder.swift
@@ -1,35 +1,35 @@
 import CoreLocation
 import Foundation
 
-final class VineyardBuilder {
-    private var name = "Test Vineyard"
-    private var address = "Chateau Margaux, 33460 Margaux"
-    private var latitude = 45.043889
-    private var longitude = -0.6687277
-    private var location = CLLocation(latitude: 45.043889, longitude: -0.6687277)
-    
-    func name(_ name: String) -> Self {
-        self.name = name
-        return self
-    }
-    func address(_ address: String) -> Self {
-        self.address = address
-        return self
-    }
-    func latitude(_ latitude: Double) -> Self {
-        self.latitude = latitude
-        return self
-    }
-    func longitude(_ longitude: Double) -> Self {
-        self.longitude = longitude
-        return self
-    }
-    func location(_ location: CLLocation) -> Self {
-        self.location = location
-        return self
-    }
-    
-    func build() -> Vineyard {
-        return Vineyard(name: name, address: address, latitude: latitude, longitude: longitude, location: location)
-    }
-}
+//final class VineyardBuilder {
+//    private var name = "Test Vineyard"
+//    private var address = "Chateau Margaux, 33460 Margaux"
+//    private var latitude = 45.043889
+//    private var longitude = -0.6687277
+//    private var location = CLLocation(latitude: 45.043889, longitude: -0.6687277)
+//    
+//    func name(_ name: String) -> Self {
+//        self.name = name
+//        return self
+//    }
+//    func address(_ address: String) -> Self {
+//        self.address = address
+//        return self
+//    }
+//    func latitude(_ latitude: Double) -> Self {
+//        self.latitude = latitude
+//        return self
+//    }
+//    func longitude(_ longitude: Double) -> Self {
+//        self.longitude = longitude
+//        return self
+//    }
+//    func location(_ location: CLLocation) -> Self {
+//        self.location = location
+//        return self
+//    }
+//    
+//    func build() -> Vineyard {
+//        return Vineyard(name: name, address: address, latitude: latitude, longitude: longitude, location: location)
+//    }
+//}
