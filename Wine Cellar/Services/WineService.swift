1dab0331893d90b9c78003d7b44350ef831b9bb5
Tim Bausch <49855076+baus8650@users.noreply.github.com>
2022-10-23 18:20:04 -0700

    Add add/remove VisualDetails methods to WineService Update Wine to VisualDetails to a one-to-many relationship Add WineTests+VisualDetails test cases

--- Wine Cellar/Services/WineService.swift
+++ Wine Cellar/Services/WineService.swift
@@ -127,6 +127,42 @@ extension WineService {
         return wine
     }
     
+    public func addVisualDetails(_ visualDetails: VisualDetails, to wine: Wine) -> Wine {
+        let originalWine = Wine(context: managedObjectContext)
+        originalWine.id = wine.id
+        originalWine.abv = wine.abv
+        originalWine.ava = wine.ava
+        originalWine.company = wine.company
+        originalWine.isFavorited = wine.isFavorited
+        originalWine.numberOwned = wine.numberOwned
+        originalWine.type = wine.type
+        originalWine.varietal = wine.varietal
+        originalWine.vintage = wine.vintage
+        originalWine.wineColor = wine.wineColor
+        
+        visualDetails.addToWine(wine)
+        coreDataStack.saveContext(managedObjectContext)
+        return wine
+    }
+    
+    public func removeVisualDetails(_ visualDetails: VisualDetails, from wine: Wine) -> Wine {
+        let originalWine = Wine(context: managedObjectContext)
+        originalWine.id = wine.id
+        originalWine.abv = wine.abv
+        originalWine.ava = wine.ava
+        originalWine.company = wine.company
+        originalWine.isFavorited = wine.isFavorited
+        originalWine.numberOwned = wine.numberOwned
+        originalWine.type = wine.type
+        originalWine.varietal = wine.varietal
+        originalWine.vintage = wine.vintage
+        originalWine.wineColor = wine.wineColor
+        
+        visualDetails.removeFromWine(wine)
+        coreDataStack.saveContext(managedObjectContext)
+        return wine
+    }
+    
     @discardableResult
     public func update(_ wine: Wine) -> Wine {
         coreDataStack.saveContext(managedObjectContext)
