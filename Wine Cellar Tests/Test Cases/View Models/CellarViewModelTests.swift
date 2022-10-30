9c0aeebfc3b8731d6494f7ccd13c67d9a1e41184
Tim Bausch <49855076+baus8650@users.noreply.github.com>
2022-10-30 12:24:41 -0700

    Add filter and sort tests to CellarViewModel

--- Wine Cellar Tests/Test Cases/View Models/CellarViewModelTests.swift
+++ Wine Cellar Tests/Test Cases/View Models/CellarViewModelTests.swift
@@ -35,4 +35,45 @@ final class CellarViewModelTests: XCTestCase {
             XCTAssertEqual(wines.count, 1)
         }.store(in: &cancellables)
     }
+    
+    func testDeleteSingleWine() throws {
+        var wineList = [Wine]()
+        var wineToDelete: Wine? = nil
+        var wineToKeep: Wine? = nil
+        let newWine1 = WineBuilder().build()
+        cellarViewModel.addWine(abv: newWine1.abv, ava: newWine1.ava!, company: newWine1.company, isFavorited: newWine1.isFavorited, numberOwned: Int16(newWine1.numberOwned), type: newWine1.type.rawValue, varietal: newWine1.varietal.rawValue, vintage: Int16(newWine1.vintage), wineColor: newWine1.wineColor.rawValue)
+        
+        let newWine2 = WineBuilder().build()
+        cellarViewModel.addWine(abv: newWine2.abv, ava: newWine2.ava!, company: newWine2.company, isFavorited: newWine2.isFavorited, numberOwned: Int16(newWine2.numberOwned), type: newWine2.type.rawValue, varietal: newWine2.varietal.rawValue, vintage: Int16(newWine2.vintage), wineColor: newWine2.wineColor.rawValue)
+        
+        cellarViewModel.$wines.sink { wines in
+            wineList = wines
+        }.store(in: &cancellables)
+        XCTAssertEqual(wineList.count, 2)
+        
+        wineToDelete = wineList.first!
+        wineToKeep = wineList.last!
+        cellarViewModel.deleteSingle(wineToDelete!)
+        XCTAssertEqual(wineList.count, 1)
+        XCTAssertEqual(wineList.first!.objectID, wineToKeep!.objectID)
+    }
+    
+    func testDeleteMultipleWines() throws {
+        var wineList = [Wine]()
+        let newWine1 = WineBuilder().build()
+        cellarViewModel.addWine(abv: newWine1.abv, ava: newWine1.ava!, company: newWine1.company, isFavorited: newWine1.isFavorited, numberOwned: Int16(newWine1.numberOwned), type: newWine1.type.rawValue, varietal: newWine1.varietal.rawValue, vintage: Int16(newWine1.vintage), wineColor: newWine1.wineColor.rawValue)
+        
+        let newWine2 = WineBuilder().build()
+        cellarViewModel.addWine(abv: newWine2.abv, ava: newWine2.ava!, company: newWine2.company, isFavorited: newWine2.isFavorited, numberOwned: Int16(newWine2.numberOwned), type: newWine2.type.rawValue, varietal: newWine2.varietal.rawValue, vintage: Int16(newWine2.vintage), wineColor: newWine2.wineColor.rawValue)
+        
+        cellarViewModel.$wines.sink { wines in
+            wineList = wines
+        }.store(in: &cancellables)
+        
+        XCTAssertEqual(wineList.count, 2)
+        
+        cellarViewModel.deleteMultiple(wineList)
+        
+        XCTAssertEqual(wineList.count, 0)
+    }
 }
